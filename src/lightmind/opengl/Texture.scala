589d25550d8f8c4ea2b27d16b27b16abcbba737a
Lukas <lukas100@cs.au.dk>
2014-07-09 14:00:48 +0200

    Added Texture class.

---
+++ src/lightmind/opengl/Texture.scala
@@ -0,0 +1,12 @@
+package lightmind.opengl
+
+import org.lwjgl.opengl.GL11._
+
+class Texture(val id: Int) {
+  var alive = true
+
+  def destroy() {
+    if (!alive) return
+    glDeleteTextures(id)
+  }
+}
